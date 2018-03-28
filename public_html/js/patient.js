0749421406de36c558f198f051337a75ed93f25b
fr33z00 <frisou76@yahoo.fr>
2018-03-28 17:40:35 +0100

    merge avec le master

--- public_html/js/patient.js
+++ public_html/js/patient.js
@@ -124,9 +124,6 @@ $(document).ready(function() {
   // prépare la réception de documents par phonecapture
   $("a.prepareReceptionDoc").on("click", function(e) {
     e.preventDefault();
-    if ($(this).hasClass('dicom'))
-    url: urlBase + '/patient/ajax/prepareEcho/',
-
     $.ajax({
       url: urlBase + '/patient/ajax/' + ($(this).hasClass('dicom') ? 'prepareEcho/' : 'prepareReceptionDoc/'),
       type: 'post',
@@ -783,6 +780,11 @@ function sendFormToReglementDiv(el) {
     success: function(data) {
       $('#newReglement').html(data);
       $.getScriptOnce(urlBase + "/js/patientScripts/" + scriptsList.reglement);
+      if (el.hasClass('editReglement')) {
+        //reinjection pour édition
+        $(".regleTarifCejour").attr('data-tarifdefaut', $(".regleTarifCejour").val());
+        $(".regleDepaCejour").attr('data-tarifdefaut',$(".regleDepaCejour").val());
+      }
       scrollTo(scrollDestination.newReglement, scrollDestination.delai);
       $(window).on("beforeunload", preventDataLoss);
       $('form').submit(function() {
